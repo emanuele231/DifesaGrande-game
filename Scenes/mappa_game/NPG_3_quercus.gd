e7e3a17df474c3c84e63dbb2fc69122854735fab
emanuele <emanueledestena24@gmail.com>
2024-02-26 17:31:35 +0100

    aggiunti 3 npg
    mancano altri + guardia civile/polizia

---
+++ Scenes/mappa_game/NPG_3_quercus.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+
+const speed = 52.0
+var distance = 200
+var direction = Vector2.LEFT
+
+var is_moving = true 
+var pause_duration = 2  
+var time_accumulator = 0  
+
+var initial_position: Vector2
+
+
+
+func _ready():
+	initial_position = position
+
+func _process(delta):
+	if is_moving:
+		var new_position = position + direction * speed * delta
+		if (new_position - initial_position).length() >= distance:
+			direction *= -1
+			new_position = position + direction * speed * delta
+			is_moving = false
+			time_accumulator = 0
+		else:
+			position = new_position
+	else:
+		time_accumulator += delta
+		if time_accumulator >= pause_duration:
+			is_moving = true
+	
+	
+
