b2bbb6402e56316cc13f3a2db0c57ca70d7d1f1b
Andrea Barbaro <andreabarbaro01@gmail.com>
2024-02-29 18:22:08 +0100

    Revert "animazioni personaggi"
    This reverts commit 81f09cd142657fb4b6a6ccb3a581fbdb74109c10.

--- Scenes/mappa_game/NPG_1_quercus.gd
+++ Scenes/mappa_game/NPG_1_quercus.gd
@@ -1,36 +1,35 @@
 extends CharacterBody2D
 
-const SPEED = 45.0
-var distance = 100
-var direction = Vector2.RIGHT
 
-var is_moving = true
-var pause_duration = 1  # Riduci la pausa dopo una collisione
-var time_accumulator = 0
+const speed = 50.0
+var distance = 75
+var direction = Vector2.UP
+
+var is_moving = true 
+var pause_duration = 2  
+var time_accumulator = 0  
+
 var initial_position: Vector2
+
+
 
 func _ready():
 	initial_position = position
 
 func _process(delta):
 	if is_moving:
-		var velocity = direction * SPEED
-		var new_position = position + velocity * delta
-		var collision = move_and_collide(velocity * delta)
-		if collision:
-			direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+		var new_position = position + direction * speed * delta
+		if (new_position - initial_position).length() >= distance:
+			direction *= -1
+			new_position = position + direction * speed * delta
 			is_moving = false
 			time_accumulator = 0
 		else:
 			position = new_position
-			if (new_position - initial_position).length() >= distance:
-				direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-				is_moving = false
-				time_accumulator = 0
 	else:
 		time_accumulator += delta
 		if time_accumulator >= pause_duration:
 			is_moving = true
-			initial_position = position  # Resetta la posizione iniziale dopo una pausa
-
+	
+	
 
--- Scenes/mappa_game/NPG_6_quercus.gd
+++ Scenes/mappa_game/NPG_6_quercus.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 45.0
+const speed = 45.0
 var distance = 100
 var direction = Vector2.RIGHT
 
-var is_moving = true
-var pause_duration = 1  # Riduci la pausa dopo una collisione
-var time_accumulator = 0
+var is_moving = true 
+var pause_duration = 5  
+var time_accumulator = 0  
 var initial_position: Vector2
 
 func _ready():
@@ -14,24 +14,19 @@ func _ready():
 
 func _process(delta):
 	if is_moving:
-		var velocity = direction * SPEED
-		var new_position = position + velocity * delta
-		var collision = move_and_collide(velocity * delta)
-		if collision:
-			direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+		var new_position = position + direction * speed * delta
+		if (new_position - initial_position).length() >= distance:
+			direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() # Modifica la direzione in modo casuale
+			new_position = position + direction * speed * delta
 			is_moving = false
 			time_accumulator = 0
 		else:
 			position = new_position
-			if (new_position - initial_position).length() >= distance:
-				direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-				is_moving = false
-				time_accumulator = 0
 	else:
 		time_accumulator += delta
 		if time_accumulator >= pause_duration:
 			is_moving = true
-			initial_position = position  # Resetta la posizione iniziale dopo una pausa
 
+	
 	
 
