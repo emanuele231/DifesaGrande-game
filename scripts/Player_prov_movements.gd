61bc3f7fc757b4b00c2ef90d53b5b6245bcd3c84
emanuele <emanueledestena24@gmail.com>
2024-03-01 10:30:45 +0100

    impostati limiti percorribili

--- scripts/Player_prov_movements.gd
+++ scripts/Player_prov_movements.gd
@@ -6,8 +6,8 @@ var input_movement = Vector2.ZERO
 
 func _process(delta):
 	var player_position = position
-	var min_limit = Vector2(-400, -300)  # Imposta i limiti minimi della mappa
-	var max_limit = Vector2(4000, 3000) 
+	var min_limit = Vector2(-500, -1000)  # Imposta i limiti minimi della mappa
+	var max_limit = Vector2(810, 500) 
 
 	player_position.x = clamp(player_position.x, min_limit.x, max_limit.x)
 	player_position.y = clamp(player_position.y, min_limit.y, max_limit.y)
