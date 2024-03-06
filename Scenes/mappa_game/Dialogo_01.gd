fe8135f037777eb8ab27b40f908bcf055b890af7
emanuele <emanueledestena24@gmail.com>
2024-03-06 16:35:48 +0100

    ora l'assessore, una volta finito il dialogo, va via

--- Scenes/mappa_game/Dialogo_01.gd
+++ Scenes/mappa_game/Dialogo_01.gd
@@ -1,7 +1,5 @@
 extends Control
 
-
-# Proprietà del dialogo
 var dialog_sequence := [
 	"Benvenuto nel bosco Difesa Grande!",
 	"Da qui, inizia la tua avventura!",
@@ -14,25 +12,32 @@ var current_dialog_index: int = -1
 var player_script = preload("res://scripts/Player_prov_movements.gd")
 var player = player_script.new()
 var _on_dialog_complete_callback = null
+var assessore_script = preload("res://Scenes/mappa_game/Assessore.gd")
+var assessore = assessore_script.new()
+var _assessore_callback = null
 signal dialogo_completato
 
 
+
+
+
+
 func _ready():
+	
 	show_next_dialog()
 	enable_player_movement()
-	
-	
-'''	
-func disable_player_movement():
-	player.can_move = false
-	'''
-	
+
+
 	
 func enable_player_movement():
 	emit_signal("dialogo_completato")
-	if _on_dialog_complete_callback != null:
+	if _on_dialog_complete_callback != null and _assessore_callback != null:
 		_on_dialog_complete_callback._on_dialog_complete()
+		_assessore_callback._on_dialogo_completato()
+
+
 		
+
 func show_next_dialog() -> void:
 	
 	if current_dialog_index >= -1:
@@ -42,10 +47,11 @@ func show_next_dialog() -> void:
 			$Label.text = dialog_sequence[current_dialog_index]
 			$Label.show()
 			player.can_move = false
+			assessore.can_move = false
 		else:
 			$Label.free()
 			enable_player_movement()
-			
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
