133a8c82fecfb0aefe546013be8e7bb10503e354
emanuele <emanueledestena24@gmail.com>
2024-03-26 13:14:29 +0100

    inserite tutte le animazioni e fixata animazione del personaggio mappa

--- Scenes/mappa_game/NPG_6_quercus.gd
+++ Scenes/mappa_game/NPG_6_quercus.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-const speed = 45.0
-var distance = 100
-var direction = Vector2.RIGHT
+
+const speed = 52.0
+var distance = 70
+var direction = Vector2.DOWN
+
 
 var is_moving = true 
-var pause_duration = 5  
+var pause_duration = 2  
 var time_accumulator = 0  
+
 var initial_position: Vector2
+
+
 
 func _ready():
 	initial_position = position
@@ -16,7 +21,7 @@ func _process(delta):
 	if is_moving:
 		var new_position = position + direction * speed * delta
 		if (new_position - initial_position).length() >= distance:
-			direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() # Modifica la direzione in modo casuale
+			direction *= -1
 			new_position = position + direction * speed * delta
 			is_moving = false
 			time_accumulator = 0
@@ -26,7 +31,6 @@ func _process(delta):
 		time_accumulator += delta
 		if time_accumulator >= pause_duration:
 			is_moving = true
-
 	
 	
 
