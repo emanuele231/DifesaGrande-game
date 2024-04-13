e61f8982fe5472687d62c2feccb1d55deb902c4e
emanuele <70099326+emanuele231@users.noreply.github.com>
2024-04-13 12:07:03 +0200

    tasto riavvia

--- Scenes/livello_1/pausa_gioco.gd
+++ Scenes/livello_1/pausa_gioco.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var riavvia = preload("res://Scenes/livello_1/lvl_1_raccolta_rifiuti.tscn")
 @onready var pannello_pausa = $"."
 var riprendi: bool = false
 
@@ -10,3 +10,8 @@ func _on_riprendi_button_down():
 	riprendi = true
 	pannello_pausa.hide()
 	Engine.time_scale = 1
+
+
+func _on_riavvia_button_down():
+	get_tree().reload_current_scene()
+	
--- Scenes/mappa_game/pannello-pausa.gd
+++ Scenes/mappa_game/pannello-pausa.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var pausa = $"."
+@onready var rigioca = preload("res://Scenes/livello_1/lvl_1_raccolta_rifiuti.tscn") as PackedScene
 var ripresa: bool = false
 
 
@@ -9,3 +10,5 @@ func _on_riprendi_button_down():
 	ripresa = true
 	pausa.hide()
 	Engine.time_scale = 1
+	
+	
