e888c8e68542ea62ea6ac3f93e6119cf3c82baa8
Andrea <andreabarbaro01@gmail.com>
2024-04-16 16:42:52 +0200

    menù

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -4,6 +4,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 
 
+func _input(event):
+	if event is InputEventJoypadButton: 
+		if event.button_index == 1 and event.pressed: 
+			_on_start_button_down()
+		elif event.button_index == 0 and event.pressed:
+			_on_esci_button_down()
+
 
 func _on_start_button_down():
 	var Start_level = preload("res://Scenes/mappa_game/mappa.tscn") as PackedScene
