41e955337ba78e7a5f674c207ef0a9434edf1a97
emanuele <emanueledestena24@gmail.com>
2024-05-27 16:50:34 +0200

    connessione dialogo con assessore_2
    can_talk di vedere lo vede

--- Scenes/mappa_game/Dialogo_02.gd
+++ Scenes/mappa_game/Dialogo_02.gd
@@ -10,19 +10,63 @@ var dialog_01_sequence := [
 	"Trova il punto esclamativo ( !!! ) bianco sulla strada",
 	"scopri che cosa minaccia queste creature!"
 ] 
-# Called when the node enters the scene tree for the first time.
+
+
+var current_dialog_index: int = -1
+var player_script = preload("res://scripts/Player_prov_movements.gd")
+var player = player_script.new()
+var assessore_script = preload("res://Scenes/mappa_game/assessore-lvl-2.gd")
+var assessore = assessore_script.new()
+var _on_dialog_complete_callback = null
+var _assessore_2_callback = null
+var _label_01_callback = null
+signal dialogo_completato
+
+var block: bool = false
+	
+	
+
 func _ready():
-	$".".hide()
+	if assessore.can_talk == true:
+		$Label_02.hide()
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+	
+func enable_player_movement():
+	emit_signal("dialogo_completato")
+	if _on_dialog_complete_callback != null and _assessore_2_callback != null and _label_01_callback != null:
+		_on_dialog_complete_callback._on_dialog_complete()
+		_assessore_2_callback._on_dialogo_completato()
+		_label_01_callback._on_dialogo_completo()
+		
 
 
-func _on_area_2d_body_entered(body):
-	pass # Replace with function body.
+		
+
+func show_next_dialog() -> void:
+	if assessore.can_talk == true:
+		$Label_02.show()
+		if current_dialog_index >= -1:
+			current_dialog_index += 1
+		
+			if current_dialog_index < dialog_01_sequence.size():
+				$Label_02.text = dialog_01_sequence[current_dialog_index]
+				$Label_02.show()
+				$Label_02.z_index = 2
+				player.can_move = false
+			else:
+				$Label_02.free()
+				enable_player_movement()
 
 
-func _on_area_2d_body_exited(body):
-	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventJoypadButton and event.is_pressed() or event is InputEventMouseButton and event.is_pressed():
+		if current_dialog_index < dialog_01_sequence.size():
+			show_next_dialog()
+			if Input.is_joy_button_pressed(JOY_BUTTON_A,JOY_BUTTON_A):
+				pass
+
+
--- Scenes/mappa_game/assessore-lvl-2.gd
+++ Scenes/mappa_game/assessore-lvl-2.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 70
 var can_move: bool = false
+var can_talk: bool = false
+signal now_you_can_talk
 
 func _ready():
 	get_parent().get_node("Player_prov/Sprite2D/Dialogo_02")._assessore_2_callback = self
@@ -18,3 +20,16 @@ func move(_delta):
 	if can_move == true:
 		var new_position = position + Vector2.DOWN * SPEED * _delta
 		position = new_position
+
+
+func _on_area_2d_body_entered(body: CharacterBody2D):
+		can_talk = true
+		emit_signal("now_you_can_talk")
+		print("ok")
+
+
+func _on_area_2d_body_exited(body: CharacterBody2D):
+	can_talk = false
+
+
+
