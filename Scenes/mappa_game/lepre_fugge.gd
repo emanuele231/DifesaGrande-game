020c61c62b51843ddb5ea46b3566d7fa0b3b81e2
emanuele <emanueledestena24@gmail.com>
2024-05-31 17:25:39 +0200

    le lepri scappano a destra

--- Scenes/mappa_game/lepre_fugge.gd
+++ Scenes/mappa_game/lepre_fugge.gd
@@ -1,23 +1,22 @@
-extends Area2D
+extends CharacterBody2D
 
 var can_move: bool = false
-var SPEED: int = 80
+var SPEED: int = 10
 
-
-
-func _on_body_entered(body: CharacterBody2D):
+func _on_scappa_body_entered(body: CharacterBody2D):
 	can_move = true
-	print("ok")
 
 
-func _on_body_exited(body: CharacterBody2D):
+func _on_scappa_body_exited(body: CharacterBody2D):
 	can_move = false
 
+
+
 func _physics_process(delta):
+	if can_move == true:
 		move(delta)
 	
 func move(delta):
-	var new_position = position + Vector2.DOWN * SPEED * delta
+	var new_position = position + Vector2.RIGHT * SPEED * delta
 	position = new_position
-
 
