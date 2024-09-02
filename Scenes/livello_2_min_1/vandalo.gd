156e4c24b69a8caaf12a37cc3ba53e01b36c3786
emanuele <emanueledestena24@gmail.com>
2024-09-02 18:46:02 +0200

    azzerato vandalo

--- Scenes/livello_2_min_1/vandalo.gd
+++ Scenes/livello_2_min_1/vandalo.gd
@@ -10,54 +10,17 @@ var schiva: bool = false
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
-func _on_rincorri_body_entered(body: CharacterBody2D):
-	can_run = true
-	player_position = body.position
-	direction = (position - player_position).normalized()
 
-func _on_rincorri_body_exited(body):
-	can_run = false
 
 func _process(delta):
 	var player_position = position
-	var min_limit = Vector2(-50, -980)  # Imposta i limiti minimi della mappa
+	var min_limit = Vector2(-50, -980)  
 	var max_limit = Vector2(1200, 600) 
-
 	player_position.x = clamp(player_position.x, min_limit.x, max_limit.x)
 	player_position.y = clamp(player_position.y, min_limit.y, max_limit.y)
 	position = player_position
 
-func _physics_process(delta):
-	if can_run:
-		if schiva:
-			direction = change_direction_90_degrees(direction)
-			schiva = false  # Reset the flag after changing direction
-		else:
-			direction = (position - player_position).normalized()
-		move(delta)
-
-func move(delta):
-	var motion = direction * speed * delta
-	position += motion
-
-
-func change_direction_90_degrees(current_direction: Vector2) -> Vector2:
-	var new_direction = current_direction.rotated(deg_to_rad(90))
-	return new_direction
-
-func _ready():
-	randomize()
 
 
 
-func _on_catturato_body_entered(body: CharacterBody2D):
-	victory = true
 
-func _on_catturato_body_exited(body):
-	victory = false
-
-func _on_changedirection_body_entered(body: CharacterBody2D):
-	schiva = true
-
-func _on_changedirection_body_exited(body):
-	schiva = false
