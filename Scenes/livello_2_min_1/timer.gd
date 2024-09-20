63ade87cd8ea74f22af61b7413583b23bff47092
emanuele <emanueledestena24@gmail.com>
2024-09-20 11:59:07 +0200

    punteggi inseriti

--- Scenes/livello_2_min_1/timer.gd
+++ Scenes/livello_2_min_1/timer.gd
@@ -5,6 +5,7 @@ var current_time: float = 300
 var timer_running: bool = false
 var end: bool = false
 
+
 func _ready():
 	$Label.z_index = 3
 	get_node("../dialogo_min_2")._timer_callback = self
@@ -26,3 +27,4 @@ func _process(delta):
 func update_timer():
 	var time_left = round(current_time)
 	$Label.text = str(time_left)
+	$"../../punteggio/punti".text = str(time_left)
