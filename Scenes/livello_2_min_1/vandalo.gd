dfe7bbb5a125d17d50c3e1d0de7e3a05da6fe439
emanuele <emanueledestena24@gmail.com>
2024-10-02 18:06:50 +0200

    animazioni sistemate
    (manca qualche fix)

--- Scenes/livello_2_min_1/vandalo.gd
+++ Scenes/livello_2_min_1/vandalo.gd
@@ -6,11 +6,8 @@ var direction: Vector2 = Vector2.ZERO
 var player_position: Vector2 = Vector2.ZERO
 var victory: bool = false
 
-
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
-
-
 
 func _process(delta):
 	var player_position = position
@@ -24,10 +21,8 @@ func _on_rincorri_body_entered(body: CharacterBody2D):
 	can_run = true
 	fugaDestra()
 
-
 func _on_rincorri_body_exited(body):
 	can_run = false
-
 
 func _physics_process(delta):
 	if can_run:
@@ -40,60 +35,64 @@ func fugaSinistra():
 	direction = Vector2.LEFT
 
 func fugaSu():
-	direction = Vector2.DOWN
-
-func fugaGiu():
 	direction = Vector2.UP
 
+func fugaGiu():
+	direction = Vector2.DOWN
 
 func move(delta):
 	var motion = direction * speed * delta
 	position += motion
 	update_animation(motion)
 
-
 func update_animation(motion: Vector2):
-	if motion.length() > 0:
-		animation_state.travel("walk")
-	else:
-		animation_state.travel("idle")
+	if motion.length() > 0 and can_run == true:
+		if direction == Vector2.RIGHT:
+			animation_state.travel("walk_right")
+		elif direction == Vector2.LEFT:
+			animation_state.travel("walk_left")
+		elif direction == Vector2.UP:
+			animation_state.travel("walk_up")
+		elif direction == Vector2.DOWN:
+			animation_state.travel("walk_down")
+	elif can_run == false:
+		if direction == Vector2.RIGHT:
+			animation_state.travel("idle_right")
+		elif direction == Vector2.LEFT:
+			animation_state.travel("idle_left")
+		elif direction == Vector2.UP:
+			animation_state.travel("idle_up")
+		elif direction == Vector2.DOWN:
+			animation_state.travel("idle_down")
 
-
+# Segnali per gestire il movimento in diverse direzioni
 func _on_rincorrisu_body_entered(body: CharacterBody2D):
 	can_run = true
 	fugaSu()
 
-
 func _on_rincorrisu_body_exited(body):
 	can_run = false
-
 
 func _on_rincorrigiu_body_entered(body: CharacterBody2D):
 	can_run = true
 	fugaGiu()
 
-
 func _on_rincorrigiu_body_exited(body):
 	can_run = false
-
 
 func _on_rincorrisinistra_body_entered(body: CharacterBody2D):
 	can_run = true
 	fugaSinistra()
 
-
 func _on_rincorrisinistra_body_exited(body):
 	can_run = false
-
 
 func _on_changedirection_body_entered(body: CharacterBody2D):
 	can_run = true
 	fugaSu()
 
-
 func _on_changedirection_body_exited(body):
 	can_run = true
-
 
 func _on_changedirectionopposite_body_entered(body: CharacterBody2D):
 	can_run = true
@@ -102,23 +101,18 @@ func _on_changedirectionopposite_body_entered(body: CharacterBody2D):
 func _on_changedirectionopposite_body_exited(body):
 	can_run = true
 
-
 func _on_lookaround_body_entered(body: CharacterBody2D):
 	can_run = true
 	fugaDestra()
 
-
 func _on_lookaround_body_exited(body):
 	can_run = true
-
 
 func _on_scappa_body_entered(body: CharacterBody2D):
 	can_run = true
 
-
 func _on_scappa_body_exited(body):
 	can_run = false
-
 
 func _on_lookaroundopposite_body_entered(body: CharacterBody2D):
 	can_run = true
