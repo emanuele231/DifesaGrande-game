d74cc5a5db78af51a979018432d530776daae372
Lola Bunny <63649992+Lola-bunny-owo@users.noreply.github.com>
2024-10-10 22:29:40 +0200

    Implementata transizione alla mappa
    La transizione alla mappa al momento è un po' lenta (perché carica la scena a run-time), però funziona ed è carina :3.

--- Scenes/scene_transition.gd
+++ Scenes/scene_transition.gd
@@ -1,16 +1,18 @@
 extends CanvasLayer
 
+@onready var box_caricamento = $Box_Caricamento
+
 func change_scene(target: String):
 
 	# Nota bene: non si puo' fare il preload della scena che viene passata in input,
 	# in quanto la stringa non e' statica. Anche volendo utilizzare ResourceLoader.load(),
 	# non va bene, dato che poi restituiremmo un oggetto di tipo PackedScene e non una stringa.
 	# Si puo' smorzare la lentezza del cambio di scena, aggiungendo un loading screen fatto bene.
-
+	box_caricamento.show()
 	$AnimationPlayer.play('dissolve')
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file(target)
-	$AnimationPlayer.play_backwards('dissolve')
+	$AnimationPlayer.play('loaded')
 	# implementare un loading screen fatto bene -> grafiche con foglie, alberi, simili.
 
 
