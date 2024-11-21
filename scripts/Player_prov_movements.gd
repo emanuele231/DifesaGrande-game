67ae53158bee8ae85693de667c66c74531860941
gmacucella <g.acucella@studenti.uniba.it>
2024-11-21 10:21:53 +0100

    Corsa
    Fuori dai minigiochi è possibile correre tenendo premuto shift.
    
    In mancanza di un'appropriata animazione per la corsa, ho lasciato l'animazione della camminata.

--- scripts/Player_prov_movements.gd
+++ scripts/Player_prov_movements.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var animazione = anim_tree.get("parameters/playback")
 
 var SPEED = 70
+var RUN_SPEED = 120 
 var input_movement = Vector2.ZERO
 var can_move: bool = false
 
@@ -15,20 +16,15 @@ func _on_dialog_complete():
 
 func _process(delta):
 	var player_position = position
-	var min_limit = Vector2(-415, -2690) # -1060 oer la zona 1
+	var min_limit = Vector2(-415, -2690) # -1060 per la zona 1
 	var max_limit = Vector2(880, 425) 
 
 	player_position.x = clamp(player_position.x, min_limit.x, max_limit.x)
 	player_position.y = clamp(player_position.y, min_limit.y, max_limit.y)
 	position = player_position
 
-
-
-
-
 func _physics_process(_delta):
-		move()
-
+	move()
 
 func move():
 	if can_move:
@@ -43,15 +39,26 @@ func move():
 		if Input.is_action_pressed("ui_down"):
 			input_movement.y = 1
 
+		# Corsa
+		var current_speed = SPEED
+		if Input.is_action_pressed("ui_shift"):  #ui_shift impostato nella mappa degli input 
+			current_speed = RUN_SPEED
+
 		if input_movement != Vector2.ZERO:
 			anim_tree.set("parameters/idle/blend_position", input_movement)
 			anim_tree.set("parameters/walk/blend_position", input_movement)
+			#anim_tree.set("parameters/run/blend_position", input_movement)
+
+			# Impostare un'animazione, per ora lascio quella della camminata
+			#if current_speed == RUN_SPEED:
+				#animazione.travel("run")
+			#else:
 			animazione.travel("walk")
-			velocity = input_movement * SPEED
+
+			velocity = input_movement.normalized() * current_speed
 
 		if input_movement == Vector2.ZERO:
 			animazione.travel("idle")
 			velocity = Vector2.ZERO
+
 		move_and_slide()
-
-
